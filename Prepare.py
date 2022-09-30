7202ed03543a44751f94c70b13e4a6ba66f208ae
Tianyu Li <ltyucb@gmail.com>
2022-09-30 14:24:35 +0800

    Build From Source WIP

--- Prepare.py
+++ Prepare.py
@@ -34,6 +34,7 @@ def build_zlib():
 
     os.system("git clone " + ZLIB_REPO + " " + ZLIB_DIR)
     os.chdir(ZLIB_DIR)
+
     os.system("git checkout " + ZLIB_VERSION + " --force")
     os.mkdir(ZLIB_BUILD_DIR)
 
@@ -42,7 +43,29 @@ def build_zlib():
 
 def build_tiff():
     LIBTIFF_VERSION= "v4.3.0"
-    # LIBTIFF_REPO =
+    LIBTIFF_REPO = "https://gitlab.com/libtiff/libtiff.git"
+    LIBTIFF_DIR = dir_path + "/ext/libtiff"
+    LIBTIFF_BUILD_DIR = LIBTIFF_DIR + "/build"
+    LIBTIFF_INSTALL_DIR = dir_path + "/ext/dist"
+    print("libtiff will be installed to " + LIBTIFF_INSTALL_DIR)
+
+    if not os.path.isdir(LIBTIFF_DIR):
+        os.system("git clone " + LIBTIFF_REPO + " " + LIBTIFF_DIR)
+    os.chdir(LIBTIFF_DIR)
+
+    os.system("git checkout " + LIBTIFF_VERSION + " --force")
+    if not os.path.isdir(LIBTIFF_BUILD_DIR):
+        os.mkdir(LIBTIFF_BUILD_DIR)
+
+    os.system("cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=" + LIBTIFF_INSTALL_DIR + "..")
+    os.system("cmake --build . --target install")
+
+    os.environ["LIBTIFF_ROOT"] = LIBTIFF_INSTALL_DIR
+
+def build_openexr():
+    OPENEXR_VERSION = "v2.4.1"
+    OPENEXR_REPO = "https://github.com/AcademySoftwareFoundation/openexr.git"
 
 # build_boost()
 # build_zlib()
+# build_tiff()
